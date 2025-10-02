307a5c8bd8dd95dda8c1409c6f42374399276f61
oshaq naimat <oshaqnaimat15@gmail.com>
2025-10-02 21:54:58 +0500

    mid

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,42 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import Values from 'values.js'
 const App = () => {
+  const [colorvalue,setColorvalue] = useState('')
+  const [mycolor,setMycolor] = useState('')
+  const [list,setList] = useState({})
+  const handleColorChange = (e) => {
+     e.preventDefault()
+     setMycolor(colorvalue)
+
+     const mylist = new Values(colorvalue).all(10)
+     console.log(mylist)
+  }
+
   return (
     <>
-    <div className="container mx-auto w-[90%] md:w-1/2 lg:w-1/3 shadow-lg rounded-md my-5 p-3">
-       <h1 className='text-center text-2xl font-semibold'>Color Generator</h1>
-       <input type="number" placeholder='e.g. 3,4,5' className='w-full outline-0 border p-1 rounded-md border-blue-400 border-[2px]' />
-       <button className='my-2 text-center bg-blue-500 active:scale-90 hover:bg-blue-600 text-white font-semibold w-full rounded-md p-1 cursor-pointer duration-200'>Generate</button>
+    <div
+    style={{
+      boxShadow: `${mycolor} 0px 15px 25px, ${mycolor} 0px 5px 10px`
+    }}
+    className={`
+    container
+     mx-auto
+     w-[90%]
+     md:w-1/2
+     lg:w-1/3
+     shadow-lg
+     rounded-md
+     my-5
+     p-3
+      `}>
+       <h1
+       style={{color : mycolor}}
+       className='text-center text-2xl font-semibold'>Color Generator</h1>
+       <input value={colorvalue} onChange={(e)=>setColorvalue(e.target.value)} type="text" placeholder='e.g. red,blue' className='w-full outline-0 border p-1 rounded-md border-blue-400 border-[2px]' />
+       <button
+       style={{backgroundColor : mycolor}}
+       onClick={handleColorChange} className='my-2 text-center bg-blue-500 active:scale-90 hover:bg-blue-600 text-white font-semibold w-full rounded-md p-1 cursor-pointer duration-200'>Generate</button>
+       
     </div>
     </>
   )
